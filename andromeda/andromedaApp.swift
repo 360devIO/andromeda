87475b9e1e80aaa42e349322b0fcc1c4c7dda4e5
nasir <nasir.siraj@softcent.eu>
2022-10-29 03:09:01 +0600

    Initial Commit

---
+++ andromeda/andromedaApp.swift
@@ -0,0 +1,17 @@
+//
+//  andromedaApp.swift
+//  andromeda
+//
+//  Created by Softcent on 29/10/22.
+//
+
+import SwiftUI
+
+@main
+struct andromedaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
